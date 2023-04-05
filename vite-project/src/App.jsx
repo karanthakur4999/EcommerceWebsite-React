3a9f5c4d6679dd211db70e69d66a39b536b2d3be
Karan Thakur <123806385+karanthakur4999@users.noreply.github.com>
2023-04-05 15:59:50 +0530

    define route for the pages

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-// import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Products from './Products';
+import Contact from './Contact';
+import Cart from './Cart';
+import SingleProduct from './SingleProduct';
+import ErrorPage from './ErrorPage';
 
 const App = () => {
   return (
     <>
-    app
-    {/* <BrowserRouter>
+    <BrowserRouter>
     <Routes>
-      <Route>
-      
-      </Route>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/Products' element={<Products/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+      <Route path='/Cart' element={<Cart/>}/>
+      <Route path='/singleproduct/:id' element={<SingleProduct/>}/>
+      <Route path='/Cart' element={<Cart/>}/>
+      <Route path='*' element={<ErrorPage/>}></Route>
     </Routes>
-    </BrowserRouter> */}
+    </BrowserRouter>
+    
     </>
   )
 }
